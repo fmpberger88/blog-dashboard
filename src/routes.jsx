e73a5cdb03a250fbac1149b91b0a53bef8404386
fmpberger88 <fabian.berger@protonmail.ch>
2024-06-24 15:18:19 +0200

    adding register and login routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./components/Root/Root.jsx";
+import Register from "./components/Register/Register.jsx";
+import Login from "./components/Login/Login.jsx";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Root />
+        element: <Root />,
+        children: [
+            {
+                path: 'register',
+                element: <Register />
+            },
+            {
+                path: 'login',
+                element: <Login />
+            }
+        ]
     }
 ]);
 
