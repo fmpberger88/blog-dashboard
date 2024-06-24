f92409d6541b0ecdc07a9ec191f46c1ae82e38e1
fmpberger88 <fabian.berger@protonmail.ch>
2024-06-24 15:52:52 +0200

    basic header styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import { NavLink, useNavigate } from 'react-router-dom';
+import styles from './Header.module.css';
 
 const Header = () => {
     const navigate = useNavigate();
@@ -11,12 +12,15 @@ const Header = () => {
     };
 
     return (
-        <header>
+        <header className={styles.headerContainer}>
             <nav>
                 <ul>
-                    <li><NavLink to="/blogs">Blogs</NavLink></li>
-                    <li><NavLink to="/create-blog">Create Blog</NavLink></li>
-                    {! token ? (
+                    <div>
+                        <li><NavLink to="/blogs">Blogs</NavLink></li>
+                        <li><NavLink to="/create-blog">Create Blog</NavLink></li>
+                    </div>
+                    <div>
+                    {!token ? (
                         <>
                             <li><NavLink to="/login">Login</NavLink></li>
                             <li><NavLink to="/register">Register</NavLink></li>
@@ -24,6 +28,7 @@ const Header = () => {
                     ) : (
                         <li><button onClick={handleLogout}>Logout</button></li>
                     )}
+                    </div>
                 </ul>
             </nav>
         </header>
