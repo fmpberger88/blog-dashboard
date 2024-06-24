644e20a9699194870976d501ffd999e9294abc8b
fmpberger88 <fabian.berger@protonmail.ch>
2024-06-24 13:27:17 +0200

    adding Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,32 @@
+import { NavLink, useNavigate } from 'react-router-dom';
+
 const Header = () => {
+    const navigate = useNavigate();
+    const token = localStorage.getItem('token');
+
+    // Delete JWT Token
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        navigate('/login');
+    };
+
     return (
-        <>
-            <p>My Header</p>
-        </>
+        <header>
+            <nav>
+                <ul>
+                    <li><NavLink to="/blogs">Blogs</NavLink></li>
+                    <li><NavLink to="/create-blog">Create Blog</NavLink></li>
+                    {! token ? (
+                        <>
+                            <li><NavLink to="/login">Login</NavLink></li>
+                            <li><NavLink to="/register">Register</NavLink></li>
+                        </>
+                    ) : (
+                        <li><button onClick={handleLogout}>Logout</button></li>
+                    )}
+                </ul>
+            </nav>
+        </header>
     )
 };
 
