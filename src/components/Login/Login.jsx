40634a79981e01beac71a93254bd551df8257400
fmpberger88 <fabian.berger@protonmail.ch>
2024-06-24 15:36:12 +0200

    update tanstack query

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import { useMutation} from "@tanstack/react-query";
 import { login } from '../../api.jsx';
+import {Link, useNavigate} from "react-router-dom";
 
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
-    const mutation = useMutation(login, {
+    const mutation = useMutation({
+        mutationFn: login,
         onSuccess: (data) => {
             localStorage.setItem('token', data.token);
             alert("Login successful!");
+            navigate('/blogs');
+
         },
         onError: (error) => {
             alert(error.message);
@@ -24,11 +29,21 @@ const Login = () => {
     return (
         <form onSubmit={handleSubmit}>
             <h1>Login</h1>
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
+            <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email" required />
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                required />
             <button type="submit" disabled={mutation.isLoading}>
                 {mutation.isLoading ? "Logging in..." : "Login"}
             </button>
+            <p>No Account yet? Go to <Link to="/register">Registration</Link></p>
         </form>
     );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,18 +1,23 @@
 import {useState} from "react";
 import {useMutation} from "@tanstack/react-query";
 import { register } from "../../api.jsx";
+import { Link, useNavigate } from "react-router-dom";
+
 
 const Register = () => {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
-    const mutation = useMutation(register, {
+    const mutation = useMutation({
+        mutationFn: register,
         onSuccess: () => {
             alert("Successfully registered!");
+            navigate('/login')
         },
         onError: (error) => {
-            alert('Registration failed!');
+            alert(error.message);
         }
     });
 
@@ -24,12 +29,31 @@ const Register = () => {
     return (
         <form onSubmit={handleSubmit}>
             <h1>Register</h1>
-            <ipnut type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" required />
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
+            <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+                required
+            />
+            <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="Email"
+                required
+            />
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                required
+            />
             <button type="submit" disabled={mutation.isLoading}>
                 {mutation.isLoading ? "Loading..." : "Register"}
             </button>
+            <p>Already an Account? Go to <Link to='/login'>Login</Link></p>
         </form>
     );
 };
