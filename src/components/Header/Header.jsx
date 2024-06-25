d4e8d47a579decf1009026f1dcc88b5a298a00d7
fmpberger88 <fabian.berger@protonmail.ch>
2024-06-25 22:46:21 +0200

    adding UsersBlogs component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,17 +17,32 @@ const Header = () => {
             <nav>
                 <ul>
                     <div>
-                        <li><NavLink to="/blogs">Blogs</NavLink></li>
-                        <li><NavLink to="/create-blog">Create Blog</NavLink></li>
+                        <li>
+                            <NavLink to="/blogs">Blogs</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/create-blog">Create Blog</NavLink>
+                        </li>
                     </div>
                     <div>
                     {!token ? (
                         <>
-                            <li><NavLink to="/login">Login</NavLink></li>
-                            <li><NavLink to="/register">Register</NavLink></li>
+                            <li>
+                                <NavLink to="/login">Login</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/register">Register</NavLink>
+                            </li>
                         </>
                     ) : (
-                        <li><button onClick={handleLogout}>Logout</button></li>
+                        <>
+                            <li>
+                                <NavLink to="/user/blogs">My Blogs</NavLink>
+                            </li>
+                            <li>
+                                <button onClick={handleLogout}>Logout</button>
+                            </li>
+                        </>
                     )}
                     </div>
                 </ul>
