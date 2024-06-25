0b3b7e2a09ec60ce87b0bc1340977b66fbe34fbe
fmpberger88 <fabian.berger@protonmail.ch>
2024-06-25 23:28:09 +0200

    adding h1 title

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -5,6 +5,7 @@ import Loading from "../Loading/Loading.jsx";
 import ErrorMessage from "../ErrorMessage/ErrorMessage.jsx";
 import styles from "./Blogs.module.css";
 import defaultImage from '/default_image.webp'
+import {MainTitle} from "../../styles.jsx";
 
 const Blogs = () => {
     const navigate = useNavigate();
@@ -23,18 +24,23 @@ const Blogs = () => {
     }
 
     return(
-        <div className={styles.blogsContainer}>
-            {data.map((blog) => (
-                <div
-                    key={blog._id}
-                    className={styles.blogsCard}
-                    onClick={() => navigate(`/blog/${blog._id}`)}
-                >
-                    <h2 className={styles.blogsTitle}><Link to={`/blog/${blog._id}`}>{blog.title}</Link></h2>
-                    {blog.image ? <img src={blog.image} alt={blog.alt} className={styles.blogsImage} /> : <img src={defaultImage} alt="default Image" className={styles.blogsImage} />}
-                </div>
-            ))}
-        </div>
+        <>
+            <MainTitle>All published Blogs</MainTitle>
+            <div className={styles.blogsContainer}>
+                {data.map((blog) => (
+                    <div
+                        key={blog._id}
+                        className={styles.blogsCard}
+                        onClick={() => navigate(`/blog/${blog._id}`)}
+                    >
+                        <h2 className={styles.blogsTitle}><Link to={`/blog/${blog._id}`}>{blog.title}</Link></h2>
+                        {blog.image ? <img src={blog.image} alt={blog.alt} className={styles.blogsImage}/> :
+                            <img src={defaultImage} alt="default Image" className={styles.blogsImage}/>}
+                    </div>
+                ))}
+            </div>
+        </>
+
     );
 };
 
