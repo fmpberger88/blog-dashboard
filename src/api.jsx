9cce6378fceb99ea365a4b059ed8d656fc39a458
fmpberger88 <fabian.berger@protonmail.ch>
2024-06-25 22:46:08 +0200

    adding publish and userblogs apis adding errorHandling

--- src/api.jsx
+++ src/api.jsx
@@ -16,52 +16,123 @@ axiosInstance.interceptors.request.use((config) => {
     return config;
 });
 
+const handleError = (error) => {
+    if (error.response) {
+        console.error(`Error: ${error.response.status} - ${error.response.data.message}`);
+        throw new Error(error.response.data.message || 'An error occurred');
+    } else if (error.request) {
+        console.error('Error: No response received from server');
+        throw new Error('No response received from server');
+    } else {
+        console.error('Error:', error.message);
+        throw new Error(error.message);
+    }
+};
+
 export const fetchBlogs = async () => {
-    const response = await axiosInstance.get('/blogs');
-    return response.data;
+    try {
+        const response = await axiosInstance.get('/blogs');
+        return response.data;
+    } catch (error) {
+        handleError(error);
+    }
 };
 
 export const fetchBlogById = async (id) => {
-    const response = await axiosInstance.get(`/blogs/${id}`);
-    return response.data;
+    try {
+        const response = await axiosInstance.get(`/blogs/${id}`);
+        return response.data;
+    } catch (error) {
+        handleError(error);
+    }
 };
 
 export const createBlog = async (blog) => {
-    const response = await axiosInstance.post('/blogs', blog);
-    return response.data;
+    try {
+        const response = await axiosInstance.post('/blogs', blog);
+        return response.data;
+    } catch (error) {
+        handleError(error);
+    }
 };
 
 export const updateBlog = async (id, blog) => {
-    const response = await axiosInstance.put(`/blogs/${id}`, blog);
-    return response.data;
+    try {
+        const response = await axiosInstance.put(`/blogs/${id}`, blog);
+        return response.data;
+    } catch (error) {
+        handleError(error);
+    }
 };
 
 export const deleteBlog = async (id) => {
-    const response = await axiosInstance.delete(`/blogs/${id}`);
-    return response.data;
+    try {
+        const response = await axiosInstance.delete(`/blogs/${id}`);
+        return response.data;
+    } catch (error) {
+        handleError(error);
+    }
 };
 
 export const fetchComments = async (blogId) => {
-    const response = await axiosInstance.get(`/comments/${blogId}`);
-    return response.data;
+    try {
+        const response = await axiosInstance.get(`/comments/${blogId}`);
+        return response.data;
+    } catch (error) {
+        handleError(error);
+    }
 };
 
 export const createComment = async (blogId, comment) => {
-    const response = await axiosInstance.post(`/comments/${blogId}`, comment);
-    return response.data;
+    try {
+        const response = await axiosInstance.post(`/comments/${blogId}`, comment);
+        return response.data;
+    } catch (error) {
+        handleError(error);
+    }
 };
 
 export const deleteComment = async (commentId) => {
-    const response = await axiosInstance.delete(`/comments/${commentId}`);
-    return response.data;
+    try {
+        const response = await axiosInstance.delete(`/comments/${commentId}`);
+        return response.data;
+    } catch (error) {
+        handleError(error);
+    }
+};
+
+export const fetchUserBlogs = async () => {
+    try {
+        const response = await axiosInstance.get('/blogs/users-blogs');
+        return response.data;
+    } catch (error) {
+        handleError(error);
+    }
+};
+
+export const publishBlog = async (id) => {
+    try {
+        const response = await axiosInstance.put(`/blogs/${id}/publish`);
+        return response.data;
+    } catch (error) {
+        handleError(error);
+    }
 };
 
 export const register = async (userData) => {
-    const response = await axios.post(`${API_URL}/register`, userData);
-    return response.data;
+    try {
+        const response = await axios.post(`${API_URL}/register`, userData);
+        return response.data;
+    } catch (error) {
+        handleError(error);
+    }
 };
 
 export const login = async (userData) => {
-    const response = await axios.post(`${API_URL}/login`, userData);
-    return response.data;
+    try {
+        const response = await axios.post(`${API_URL}/login`, userData);
+        return response.data;
+    } catch (error) {
+        handleError(error);
+    }
 };
