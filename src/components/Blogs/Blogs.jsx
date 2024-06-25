650485b437053794d342019daa8e96dca9358a4a
fmpberger88 <fabian.berger@protonmail.ch>
2024-06-25 07:53:38 +0200

    styling

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,9 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchBlogs } from "../../api.jsx";
+import { Link } from "react-router-dom";
+import Loading from "../Loading/Loading.jsx";
+import ErrorMessage from "../ErrorMessage/ErrorMessage.jsx";
+import styles from "./Blogs.module.css";
+import defaultImage from '/default_image.webp'
 
 const Blogs = () => {
+
+    const { data, error, isLoading } = useQuery({
+        queryKey: ['blogs'],
+        queryFn: fetchBlogs,
+    })
+
+    if (isLoading) {
+        return <Loading />
+    }
+
+    if (error) {
+        return <ErrorMessage message={error.message} />;
+    }
+
     return(
-        <>
-            <h1>Blogs</h1>
-        </>
+        <div className={styles.blogsContainer}>
+            {data.map((blog) => (
+                <div key={blog._id} className={styles.blogsCard}>
+                    <h2 className={styles.blogsTitle}><Link to={`/blog/${blog._id}`}>{blog.title}</Link></h2>
+                    {blog.image ? <img src={blog.image} alt={blog.alt} className={styles.blogsImage} /> : <img src={defaultImage} alt="default Image" className={styles.blogsImage} />}
+                </div>
+            ))}
+        </div>
     );
 };
 
